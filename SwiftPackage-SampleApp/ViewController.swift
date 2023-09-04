6556a458b6308051f8cfe14c6d3b2d6b638c77f7
rshimokura <rshimokura@itrealize.co.jp>
2023-09-04 17:29:49 +0900

    Initial Commit

---
+++ SwiftPackage-SampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftPackage-SampleApp
+//
+//  Created by Shimokura on 2023/09/04.
+//
+
+import UIKit
+import Sample_SwiftPackage
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Simba".kingdom())
+    }
+
+
+}
+
